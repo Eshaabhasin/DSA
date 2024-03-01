063acf3daedb597aabcb3694d101db70aca5d624
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2024-03-01 11:48:42 +0530

    Add files via upload

---
+++ printalldivisors.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void divisor(int n){
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
+            cout<<i;
+        }
+    }
+}
+   
+int main(){
+    int n;
+    cin>>n;
+   divisor(n);
+}
